ac7f793088392cf5e2bb6abae46e778f3978f019
unknown <sralkiswani1@catamount.wcu.edu>
2022-05-05 08:53:11 -0400

    Header Complete

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAsu7guwsd9W4G5xyiMuBEd-8yzkUzGuw",
+    authDomain: "clone-275c6.firebaseapp.com",
+    projectId: "clone-275c6",
+    storageBucket: "clone-275c6.appspot.com",
+    messagingSenderId: "548824937038",
+    appId: "1:548824937038:web:e480c857f28d41ab26c91f"
+};
