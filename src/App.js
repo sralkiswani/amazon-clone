4f908c85bf382e9af12a1297d747968ccd236937
unknown <Samer.alkiswani@jacks.sdstate.edu>
2022-05-05 12:04:11 -0400

    Initial checkout page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import './App.css';
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      {/*<h1>Hello Worldsasasa</h1>*/}
-        <Header />
-        <Home />
-    </div>
-  );
+    return (
+        // BEM
+        <Router>
+            <div className="app">
+
+                {/*<Switch>*/}
+
+                    {/*<Route path="/">*/}
+                    {/*    <Header />*/}
+                    {/*    <Home/>*/}
+                    {/*</Route>*/}
+
+                {/*</Switch>*/}
+
+                <Routes>
+                    <Route path="/checkout" element={ [<Header />, <h1>I am a checkout page</h1>]} />
+                    <Route path="/" element={[<Header />, <Home />]}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
